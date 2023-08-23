41992e97858f3a06282a1f8686d3565bfd172def
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2023-08-24 04:33:00 +0900

    feat(View): 코치 이름 입력 문구 출력 기능

--- src/main/java/menu/View.java
+++ src/main/java/menu/View.java
@@ -4,4 +4,8 @@ public class View {
     public void printStartComment() {
         System.out.println("점심 메뉴 추천을 시작합니다.");
     }
+
+    public void printCoachNameInputComment() {
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+    }
 }
