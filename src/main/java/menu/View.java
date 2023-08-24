5abbde8af465248c9eb60f3641b8bf62724c3841
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2023-08-24 19:45:36 +0900

    feat(View): 에러 문구 출력 기능 추가

--- src/main/java/menu/View.java
+++ src/main/java/menu/View.java
@@ -8,4 +8,12 @@ public class View {
     public void printCoachNameInputComment() {
         System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
     }
+
+    public void printHateMenuInputComment(String coachName) {
+        System.out.println(coachName + "(이)가 못 먹는 메뉴를 입력해 주세요.");
+    }
+
+    public void printErrorMessage(String errorMessage) {
+        System.out.println("[ERROR] " + errorMessage);
+    }
 }
